4f4e9af95ef0935d303c5cd316588cd6acf4a879
Ernest Mwinchumu <gmem200197@gmail.com>
2023-07-13 16:33:03 +0300

    Fixing Github Actions build issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,11 +6,11 @@ import Footer from "../components/footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <>
+      <body>
         <NavBar />
         <main>{children}</main>
         <Footer />
-      </>
+      </body>
     </html>
   );
 }
